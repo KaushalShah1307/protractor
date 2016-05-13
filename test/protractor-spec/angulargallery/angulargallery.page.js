4672182e997a3a62a099c93abff2240121f52b5b
U-FORBES\kshah <kshah@WS-63361.forbes.com>
2016-05-13 18:40:16 -0400

    Initialized new check for Angular Galleries

---
+++ test/protractor-spec/angulargallery/angulargallery.page.js
@@ -0,0 +1,23 @@
+/**
+ * Created by kshah on 5/13/2016.
+ */
+var angularGalleryPage = function() {
+	this.get = function() {
+		browser.get('/pictures/mli45fdllh/1-maria-sharapova/?view=angular_gallery');
+		browser.executeAsyncScript(function() {
+			callback = arguments[0];
+			callback($('body').injector().get('GalleryAppState').page_data.gallery);
+		}).then(function(result) {
+			this.page_data = result;
+		}).then(function() {
+			browser.current_page = this;
+		});
+		browser.getCurrentUrl().then(function(url) {
+			browser.current_url = url;
+			globals.pagesChecked.push(url);
+		});
+	};
+
+	this.adsService = 'GalleryAdsService';
+};
+module.exports = angularGalleryPage;
