4841f5d2244f9bdf90c44d4faca7772801947933
U-FORBES\kshah <kshah@forbes.com>
2016-05-13 19:45:50 -0400

    Added a new tracking file for the angular galleries

---
+++ test/protractor-spec/angulargallery/angulargallery-tracking.spec.js
@@ -0,0 +1,33 @@
+/**
+ * Created by kshah on 5/13/2016.
+ */
+describe('Angular Gallery', function() {
+	var currentUrl;
+
+	beforeAll(function(done) {
+		browser.getCurrentUrl().then(function(url) {
+			currentUrl = url;
+			done();
+		});
+	});
+
+	describe('Tracking', function() {
+		describe('Fast Pixel', function() {
+			var trackingPixel;
+
+			beforeAll(function(done) {
+				trackingPixel = $('img[src*="fast.forbes.com"]');
+				trackingPixel.getAttribute('src').then(function(src) {
+					trackingPixel.srcString = src;
+					done();
+				});
+			});
+
+			it ('should have the correct parameters', function() {
+				expect(globals.getParam(trackingPixel.srcString, 'su') + '/').toEqual(currentUrl.replace(browser.baseUrl,"http://www.forbes.com/"));
+				expect(globals.getParam(trackingPixel.srcString, 'ch')).toEqual(browser.current_page.page_data.channel.omnitureChannel.toLowerCase());
+				expect(globals.getParam(trackingPixel.srcString, 'se')).toEqual(browser.current_page.page_data.channel.omnitureSection.toLowerCase());
+			});
+		});
+	});
+});
