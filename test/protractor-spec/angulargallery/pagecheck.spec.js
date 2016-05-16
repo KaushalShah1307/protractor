dfb8da18a4ea3f65edd6312bc33d1f196e2b9ff3
U-FORBES\kshah <kshah@forbes.com>
2016-05-16 11:23:20 -0400

    Check for next and prev gallery slides in the carousal

--- test/protractor-spec/angulargallery/pagecheck.spec.js
+++ test/protractor-spec/angulargallery/pagecheck.spec.js
@@ -12,3 +12,24 @@ describe('Angular Gallery', function() {
 
 	globals.checkAds(galleryPage.adsService);
 });
+
+describe('Gallery Carousal', function () {
+
+	var beforeImage = "http://specials-images.forbesimg.com/imageserve/479783114/1200x0.jpg?fit=scale&background=000000";
+	var afterImage = "http://specials-images.forbesimg.com/imageserve/480371870/1200x0.jpg?fit=scale&background=000000";
+
+	it('Click should take user to next slide', function() {
+		expect(activeImage.getAttribute('background-image: url')).toBe(beforeImage);
+		next.click();
+		expect(activeImage.getAttribute('background-image: url')).toBe(afterImage);
+	});
+
+	it('clicks should take user to the previous slide', function() {
+		next.click();
+		expect(activeImage.getAttribute('background-image: url')).toBe(afterImage );
+		prev.click();
+		expect(activeImage.getAttribute('background-image: url')).toBe(beforeImage);
+	})
+
+});
+
