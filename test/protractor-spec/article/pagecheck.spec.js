5c29425d6aacd395b888d7d47ce72e07aba594c2
U-FORBES\kshah <kshah@forbes.com>
2016-05-20 20:36:11 -0400

    Drop Forbes Beta cookie for value A on the page to avoid freaking beta cookies

--- test/protractor-spec/article/pagecheck.spec.js
+++ test/protractor-spec/article/pagecheck.spec.js
@@ -2,17 +2,18 @@ var ArticlePage = require('./article.page.js'),
 	articlePage = new ArticlePage('/sites/gordonkelly/2015/09/19/apple-ios-9-secrets/');
 
 describe('Article', function() {
-	// beforeAll(function(done) {
-	// 	var request = require('request');
-	// 	request('http://192.168.16.24/forbesapi/content/all.json?retrievedfields=uri&types=blogslide,blog&limit=1', function(error,response,body) {
-	// 		if (!error && response.statusCode == 200) {
-	// 			articlePage.url = JSON.parse(body).contentList[0].uri.replace('http://www.forbes.com','');
-	// 			done();
-	// 		}
-	// 	});
-	// });
+
+	it('should mock the cookie for forbesbeta = A', function() {
+		var mock_code = function () {
+			angular.module('httpBackendMock', ['ngMockE2E', 'ngCookies'])
+				.run(function ($httpBackend, $cookies) {
+					$cookies.fobesbeta = 'A';
+				});
+		}
+		});
 
 	it('should get the page', function() {
+		articlePage.addMockModule('httpBackendMock', mock_code);
 		articlePage.get();
 	});
 
