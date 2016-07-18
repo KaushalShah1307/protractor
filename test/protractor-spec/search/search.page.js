3f997dbd8eac3ad0c67e4b3ade8380a6409791f8
U-FORBES\kshah <kshah@forbes.com>
2016-07-18 17:16:00 -0400

    Initial Search page setup

--- test/protractor-spec/search/search.page.js
+++ test/protractor-spec/search/search.page.js
@@ -1,3 +1,23 @@
 /**
  * Created by kshah on 7/18/2016.
  */
+var searchPage = function() {
+	this.get = function() {
+		browser.get('/search/');
+		browser.executeAsyncScript(function() {
+			callback = arguments[0];
+			callback($('body').injector().get('SearchAppState').page_data.gallery);
+		}).then(function(result) {
+			this.page_data = result;
+		}).then(function() {
+			browser.current_page = this;
+		});
+		browser.getCurrentUrl().then(function(url) {
+			browser.current_url = url;
+			globals.pagesChecked.push(url);
+		});
+	};
+
+	this.adsService = 'SearchAdsService';
+};
+module.exports = searchPage;
