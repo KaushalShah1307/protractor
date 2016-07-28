179b51185788e633dba69bd51a0f7ee996d83976
U-FORBES\kshah <kshah@forbes.com>
2016-07-28 18:00:57 -0400

    enable the check for search page

--- protractor.conf.js
+++ protractor.conf.js
@@ -81,9 +81,9 @@ exports.config = {
 //		'test/protractor-spec/csf/**/*.spec.js',
 //		'test/protractor-spec/channelsections/*pagecheck.spec.js',
 //		'test/protractor-spec/channelsections/**/*.spec.js',
-		'test/protractor-spec/angulargallery/*pagecheck.spec.js',
-		'test/protractor-spec/angulargallery/**/*.spec.js',
-//		'test/protractor-spec/search/*pagecheck.spec.js'
+//		'test/protractor-spec/angulargallery/*pagecheck.spec.js',
+//		'test/protractor-spec/angulargallery/**/*.spec.js',
+		'test/protractor-spec/search/*pagecheck.spec.js'
 	],
 	onPrepare: function() {
 		var FbsReporter = require('./reporter/custom-reporter.js');
