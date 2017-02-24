c3c41c7ca5f3fc9f95931dfd3fb7e95292c518c8
Kaushal Shah <kshah@forbes.com>
2017-02-24 14:00:31 -0500

    added suite of tracking tests on search page

--- test/protractor-spec/search/search-tracking.spec.js
+++ test/protractor-spec/search/search-tracking.spec.js
@@ -1,3 +1,93 @@
 /**
  * Created by kshah on 2/24/2017.
  */
+
+describe('Search', function() {
+	var currentUrl;
+
+	beforeAll(function(done) {
+		browser.getCurrentUrl().then(function(url) {
+			currentUrl = url;
+			done();
+		});
+	});
+
+	describe('Search Tracking:', function() {
+		describe('Fast Pixel', function() {
+			var trackingPixel;
+
+			beforeAll(function(done) {
+				trackingPixel = $('img[src*="fast.forbes.com"]');
+				trackingPixel.getAttribute('src').then(function(src) {
+					trackingPixel.srcString = src;
+					done();
+				});
+			});
+
+			it ('should have the correct parameters', function() {
+				//expect(globals.getParam(trackingPixel.srcString, 'su')).toEqual(currentUrl.replace(browser.baseUrl, "http://www-staging.forbes.com/"));
+				expect(globals.getParam(trackingPixel.srcString, 'ch')).toEqual(browser.current_page.page_data.displayChannel);
+                expect(globals.getParam(trackingPixel.srcString, 'se')).toEqual(browser.current_page.page_data.displaySection);
+			});
+		});
+
+		describe('Google Analytics', function() {
+			var dataLayer;
+			beforeAll(function() {
+				browser.executeScript(function() {
+					return dataLayer[0];
+				}).then(function(result) {
+					dataLayer = result;
+				});
+			});
+
+			it('should pass the right custom parameters', function() {
+				// expect(dataLayer.brandvoice).toBe(null);
+                //(expect(dataLayer.DFPSite).toMatch('fdc.forbes' || 'fdcmobile'));
+                expect(dataLayer.DFPZone).toEqual('article-d');
+				expect(dataLayer.author).toEqual('Lewis DVorkin');
+				expect(dataLayer.channel).toEqual('business');
+                expect(dataLayer.section).toEqual('lewisdvorkinblog');
+                expect(dataLayer.hashtags).toEqual('boxoffice');
+				expect(dataLayer.slot).toEqual('none');
+				expect(dataLayer.site).toEqual('lewisdvorkin');
+                //expect(dataLayer.doNotPaginate).toEqual('donotpaginate');
+				
+
+			});
+		});
+        
+        describe('SimpleReach', function() {
+            var reachpixel;
+            
+            beforeAll(function(done) {
+                reachpixel = $('script#simplereach-script[src*="d8rk54i4mohrb.cloudfront.net"]');
+                reachpixel.getAttribute('src').then(function(src) {
+                    reachpixel.srcString = src;
+                    done();
+                });      
+            });
+            
+            it('should load the SimpleReach script', function() {
+                expect(reachpixel.length > 1);
+            });
+        });
+        
+        describe('Comscore', function() {
+            var comscorepixel;
+            
+            beforeAll(function(done) {
+                comscorepixel = $('script[src*="b.scorecardresearch.com/beacon.js"]');
+                comscorepixel.getAttribute('src').then(function(src) {
+                    comscorepixel.srcString = src;
+                    done();
+                });      
+            });
+            
+            it('should load the ComScore script', function() {
+                expect(comscorepixel.length > 1);
+            });
+        });        
+        
+	});
+});
