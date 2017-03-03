431b553d1dc2aca6d181e473c58fcad8fd0e49d8
Kaushal Shah <kshah@forbes.com>
2017-03-02 19:46:35 -0500

    cleanup for pagecheck spec file for angular gallery suite

--- test/protractor-spec/angulargallery/pagecheck.spec.js
+++ test/protractor-spec/angulargallery/pagecheck.spec.js
@@ -8,39 +8,16 @@ describe('Angular Gallery', function() {
 	it('should get the page', function() {
 		galleryPage.get();
 	});
+    
+    it('should have the title of the slide', function(){
+        expect(element(by.className('slide-info')).getText()).toEqual('1. Maria Sharapova'); 
+    });
+    
+    it('should have the correct slide progress numbers', function(){
+        expect(element(by.className('gallery-progress-numbers')).getText()).toEqual('2 of 11'); 
+    });
 
 //	globals.generalCheck();
 
 	globals.checkAds(galleryPage.adsService);
 });
-/*
-describe('Gallery Carousal', function () {
-
-	var beforeImage = "http://specials-images.forbesimg.com/imageserve/479783114/1200x0.jpg?fit=scale&background=000000";
-	var afterImage = "http://specials-images.forbesimg.com/imageserve/480371870/1200x0.jpg?fit=scale&background=000000";
-//	var activeImage = element.by.css('background-image');
-
-	it('Click should take user to next slide', function() {
-		expect(beforeImage).toEqual(browser.current_page.page_data.slides[1].image_url);
-		element(by.css('.slick-next').click());
-		expect(afterImage).toEqual(browser.current_page.page_data.slides[2].image_url);
-	});
-
-	it('Click should take user to the previous slide', function() {
-		expect(afterImage).toEqual(browser.current_page.page_data.slides[2].image_url);
-		element(by.css('.slick-prev').click());
-		expect(beforeImage).toEqual(browser.current_page.page_data.slides[1].image_url);
-	})
-
-});
-
-describe('Parent article for the gallery', function(){
-
-	it('should redirect to the article page', function(){
-		element(by.id('gallery-link').click());
-		browser.waitForAngular();
-		expect(browser.driver.getCurrentUrl()).toEqual("http://www.forbes.com/sites/kurtbadenhausen/2015/08/12/the-worlds-highest-paid-female-athletes-2015/");
-	});
-
-});
-*/
