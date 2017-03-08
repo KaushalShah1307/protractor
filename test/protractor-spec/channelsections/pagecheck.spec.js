0a2afbc7d0fb04b4a1aca69c2cee0eeff9d364dc
Kaushal Shah <kshah@forbes.com>
2017-03-08 13:32:14 -0500

    small visual checks added to the Channel Section PageCheck spec

--- test/protractor-spec/channelsections/pagecheck.spec.js
+++ test/protractor-spec/channelsections/pagecheck.spec.js
@@ -4,6 +4,18 @@ describe('CSR', function() {
 	it('should get the page', function() {
 		csrPage.get();
 	});
+    
+    it('should have the channel/section name', function() {
+       expect(element(by.className('channel-name')).getText()).toEqual('LEADERSHIP'); 
+    });
+    
+    it('should have the channel/section name in the stream block', function() {
+       expect(element(by.className('csr-common-stream-header')).getText()).toEqual('LEADERSHIP NOW'); 
+    });
+    
+    it('should have the load-more button on the steam', function() {
+       var loadMore = element(by.className('load-more-bg')); 
+    });
 
 	globals.generalCheck();
 
