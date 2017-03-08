69a6cba1291b323fdb69d7f33b7e6f1fb5e66d36
Kaushal Shah <kshah@forbes.com>
2017-03-08 14:08:51 -0500

    setting up the specs for homepage redesign project

---
+++ test/protractor-spec/homepage/home.page.js
@@ -0,0 +1,20 @@
+var HomePage = function() {
+  this.get = function() {
+    browser.get('/home_usa/', 5000);
+	browser.executeAsyncScript(function() {
+		callback = arguments[0];
+		callback($('body').injector().get('CsfAppState').page_data.channel);
+	}).then(function(result) {
+		this.page_data = result;
+	}).then(function() {
+		browser.current_page = this;
+	});
+	browser.getCurrentUrl().then(function(url) {
+		browser.current_url = url;
+		globals.pagesChecked.push(url);
+	});
+  };
+
+  this.adsService = 'CsfAdService';
+};
+module.exports = HomePage;
