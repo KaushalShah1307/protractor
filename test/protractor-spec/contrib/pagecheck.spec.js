079166249a988b58050282cb0f094764f0d02cec
Kaushal Shah <kshah@forbes.com>
2017-03-08 16:16:25 -0500

    added checks for the About section on the contrib hp

--- test/protractor-spec/contrib/pagecheck.spec.js
+++ test/protractor-spec/contrib/pagecheck.spec.js
@@ -4,6 +4,14 @@ describe('ContribHP', function() {
 	it('should get the page', function() {
 		contribPage.get();
 	});
+    
+    it('should have pageviews in the stream', function() {
+        expect(element(by.className('article-views')).getText().length > 0); 
+    });
+    
+    it('should have the About section', function() {
+        expect(element(by.className('author-description')).getText().length > 0); 
+    });
 
 	globals.generalCheck();
 
