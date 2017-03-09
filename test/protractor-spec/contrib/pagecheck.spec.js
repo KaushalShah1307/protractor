91e4f28e5561faab638066ee4123f3d7c8e35077
Kaushal Shah <kshah@forbes.com>
2017-03-09 14:23:31 -0500

    added few more checks for the contrib homepage suite

--- test/protractor-spec/contrib/pagecheck.spec.js
+++ test/protractor-spec/contrib/pagecheck.spec.js
@@ -25,11 +25,24 @@ describe('ContribHP', function() {
         expect(element(by.className('trending-reason')).getText().length > 0); 
     });
     
+/*    it('should click on the author from the Our Contributor section', function() {
+        element.all(by.repeater('contributor in author.contributors').row(0)).click();
+        element(by.className('close-btn')).click();
+    });    
+    
+    it('should have archive/latest menu on the stream', function() {
+        var archiveMenu = element(by.className('toggle-archive'));
+        expect(archiveMenu.getText()).toEqual('ARCHIVE');
+        archiveMenu.click();
+        expect(archiveMenu.getText()).toEqual('LATEST');  
+    });
+ */   
     it('should have the what-is-this blurb for Brandvoice', function() {
         var bvBlurb = element(by.className('what-is-this'));
         expect(bvBlurb).toBeTruthy();
         bvBlurb.click();
         expect(element(by.className('brandvoice-explained'))).toBeTruthy();
+        element(by.className('close-button')).click();
     });
     
 	globals.generalCheck();
