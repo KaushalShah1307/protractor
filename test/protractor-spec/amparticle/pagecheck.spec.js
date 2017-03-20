f5b1718f8c1091b7caf56caafc54d988b956a1eb
Kaushal Shah <kshah@forbes.com>
2017-03-20 17:52:23 -0400

    Added checks for headline, contribname and usertype for AMP articles

--- test/protractor-spec/amparticle/pagecheck.spec.js
+++ test/protractor-spec/amparticle/pagecheck.spec.js
@@ -9,6 +9,14 @@ describe('AMP Article Page', function() {
 	it('should get the page', function() {
 		ampArticle.get();
 	});
+    
+    it('should have the article headline', function() {
+        expect(element(by.css('.article-header.inner-contain>h1')).getText()).toEqual('Inside Forbes: The Unstoppable Force That Will Change The News Industry Forever'); 
+    });
+    
+    it('should have the contributors name and type', function() {
+        expect(element(by.className('user')).getText()).toEqual('Lewis DVorkin , FORBES STAFF'); 
+    });
 
 //	globals.generalCheck();
 
