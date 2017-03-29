e0cb5ff04951c527cbf6d2f8d714d4adcc6b14d3
Kaushal Shah <kshah@forbes.com>
2017-03-29 18:15:48 -0400

    newsletters home checks

--- test/protractor-spec/newsletters/pagecheck.spec.js
+++ test/protractor-spec/newsletters/pagecheck.spec.js
@@ -3,6 +3,7 @@ var Newsletters = require('./newsletters.page.js'),
 
 beforeEach(function(){
     browser.ignoreSynchronization = true;
+    // add the cookies here
 });
 
 describe('Newsletters Home Page', function() {
@@ -10,5 +11,17 @@ describe('Newsletters Home Page', function() {
 	it('should get the page', function() {
 		newsletters.get();
 	});
+    
+    it('should have locks in front of articles', function() {
+        expect(element(by.className('lock')).isDisplayed()).toBeTruthy(); 
+    });
+    
+    it('should have special report module', function() {
+        expect(element(by.className('special-reports')).isDisplayed()).toBeTruthy(); 
+    });
+    
+    it('should have the guru section', function() {
+        expect(element(by.className('gurus')).isDisplayed()).toBeTruthy(); 
+    });
     
 });
