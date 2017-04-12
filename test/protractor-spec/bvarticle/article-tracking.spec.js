bc2b97c1fb7735ec4f702ed88943f48ede7c6338
Kaushal Shah <kshah@forbes.com>
2017-04-12 17:49:22 -0400

    finally got SR tracking working for BV articles

--- test/protractor-spec/bvarticle/article-tracking.spec.js
+++ test/protractor-spec/bvarticle/article-tracking.spec.js
@@ -84,6 +84,14 @@ describe('BrandVoice Article', function() {
                 expect(reachpixel.length > 1);
             });
             
+            it('should pass the right custom parameters', function() {
+                expect(browser.executeScript('return window.__reach_config.authors;')).toEqual('IBM Contributor'); 
+                expect(browser.executeScript('return window.__reach_config.channels;')).toEqual('leadership'); 
+                expect(browser.executeScript('return window.__reach_config.date;')).toEqual('2017-03-30T13:30:00.000Z'); 
+                expect(browser.executeScript('return window.__reach_config.pid;')).toEqual('50e4a8434240cf5c4b000009'); 
+                expect(browser.executeScript('return window.__reach_config.title;')).toEqual('How CDOs Can Use Cognitive Computing To Transform Their Businesses {Q&A}'); 
+            });
+            
         });
         
         describe('Comscore', function() {
