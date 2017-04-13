74998397ca05742e4182beb6a877fae3eeae091a
Kaushal Shah <kshah@forbes.com>
2017-04-13 13:06:34 -0400

    Added tracking spec for homepage

---
+++ test/protractor-spec/homepage/homepage-tracking.spec.js
@@ -0,0 +1,92 @@
+describe('Homepage', function() {
+	var currentUrl;
+
+	beforeAll(function(done) {
+		browser.getCurrentUrl().then(function(url) {
+			currentUrl = url;
+			done();
+		});
+	});
+
+	describe('Tracking on Homepage:', function() {
+		describe('Fast Pixel', function() {
+			var trackingPixel;
+
+			beforeAll(function(done) {
+				trackingPixel = $('img[src*="fast.forbes.com"]');
+				trackingPixel.getAttribute('src').then(function(src) {
+					trackingPixel.srcString = src;
+					done();
+				});
+			});
+
+			it ('should have the correct parameters', function() {
+				//expect(globals.getParam(trackingPixel.srcString, 'su')).toEqual(currentUrl.replace(browser.baseUrl, "http://www-staging.forbes.com/"));
+				//expect(globals.getParam(trackingPixel.srcString, 'i')).toEqual(browser.current_page.page_data.naturalId);
+				//expect(globals.getParam(trackingPixel.srcString, 'at')).toEqual(browser.current_page.page_data.blogType);
+				//expect(globals.getParam(trackingPixel.srcString, 'ch')).toEqual(browser.current_page.page_data.displayChannel);
+				//expect(globals.getParam(trackingPixel.srcString, 'ci')).toEqual(browser.current_page.page_data.authors[0].naturalId);
+			});
+		});
+
+		describe('Google Analytics', function() {
+			var dataLayer;
+			beforeAll(function() {
+				browser.executeScript(function() {
+					return dataLayer[0];
+				}).then(function(result) {
+					dataLayer = result;
+				});
+			});
+
+			it('should pass the right custom parameters', function() {
+                //expect((dataLayer.DFPSite).toEqual('fdc.forbes') || (dataLayer.DFPSite).toEqual('fdcmobile'));
+                expect(dataLayer.DFPZone).toEqual('home');
+				expect(dataLayer.channel).toEqual('home');
+				expect(dataLayer.pageType).toEqual('csf');
+                expect(dataLayer.section).toEqual('main');
+			});
+		});
+        
+        describe('SimpleReach', function() {
+            var reachpixel;
+            
+            beforeAll(function(done) {
+                reachpixel = $('script#simplereach-script[src*="d8rk54i4mohrb.cloudfront.net"]');
+                reachpixel.getAttribute('src').then(function(src) {
+                    reachpixel.srcString = src;
+                    done();
+                });      
+            });
+            
+            it('should load the SimpleReach script', function() {
+                expect(reachpixel.length > 1);
+            });
+            
+            it('should pass the right custom parameters', function() {
+                expect(browser.executeScript('return window.__reach_config.channels[0];')).toEqual('home');
+                expect(browser.executeScript('return window.__reach_config.pid;')).toEqual('50e4a8434240cf5c4b000009'); 
+                expect(browser.executeScript('return window.__reach_config.title;')).toEqual('Forbes'); 
+                expect(browser.executeScript('return window.__reach_config.url;')).toEqual('https://www.forbes.com/'); 
+            });
+            
+        });
+        
+        describe('Comscore', function() {
+            var comscorepixel;
+            
+            beforeAll(function(done) {
+                comscorepixel = $('script[src*="b.scorecardresearch.com/beacon.js"]');
+                comscorepixel.getAttribute('src').then(function(src) {
+                    comscorepixel.srcString = src;
+                    done();
+                });      
+            });
+            
+            it('should load the ComScore script', function() {
+                expect(comscorepixel.length > 1);
+            });
+        });        
+        
+	});
+});
