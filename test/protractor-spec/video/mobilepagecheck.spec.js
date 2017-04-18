988eb4d0a6a2c8bfe966bd07a4c0a2e30934c5e0
Kaushal Shah <kshah@forbes.com>
2017-04-18 18:43:21 -0400

    all sections covered for the video homepage

--- test/protractor-spec/video/mobilepagecheck.spec.js
+++ test/protractor-spec/video/mobilepagecheck.spec.js
@@ -12,6 +12,14 @@ describe('Video Home Page', function() {
 		video.get();
 	});
     
+	it('should have the page header', function() {
+		expect(element(by.tagName('header')).isPresent()).toBe(true);
+	});
+    
+	it('should have the page footer', function() {
+		expect(element(by.id('footer_links')).isPresent()).toBe(true);
+	});
+    
 	it('should have the featured video', function() {
 		expect(element(by.id('brightcove_perform_0_html5_api')).isPresent()).toBe(true);
 	});
@@ -30,6 +38,34 @@ describe('Video Home Page', function() {
     
 	it('should have the social sharing icons', function() {
 		expect(element(by.className('social')).isPresent()).toBe(true);
+	});
+    
+	it('should have the recommended section', function() {
+		expect(element(by.className('more_videos row flexslider-container')).isPresent()).toBe(true);
+	});
+    
+	it('should have the featured section', function() {
+		expect(element(by.className('featured rule row')).isPresent()).toBe(true);
+	});
+    
+	it('should have the most-popular section', function() {
+		expect(element(by.className('most_popular rule row load_more_container')).isPresent()).toBe(true);
+	});
+    
+	it('should have the latest-video section', function() {
+		expect(element(by.className('video_lower row')).isPresent()).toBe(true);
+	});
+    
+	it('should have the video search section', function() {
+		expect(element(by.className('search video_search_control clearfix')).isPresent()).toBe(true);
+	});
+    
+	it('should have the featured-playlists section', function() {
+		expect(element(by.className('featured_playlists')).isPresent()).toBe(true);
+	});
+    
+	it('should have the most-read section', function() {
+		expect(element(by.className('most_popular resp_hide')).isPresent()).toBe(true);
 	});
     
 });
--- test/protractor-spec/video/pagecheck.spec.js
+++ test/protractor-spec/video/pagecheck.spec.js
@@ -12,6 +12,14 @@ describe('Video Home Page', function() {
 		video.get();
 	});
     
+	it('should have the page header', function() {
+		expect(element(by.tagName('header')).isPresent()).toBe(true);
+	});
+    
+	it('should have the page footer', function() {
+		expect(element(by.tagName('footer')).isPresent()).toBe(true);
+	});
+    
 	it('should have the featured video', function() {
 		expect(element(by.id('brightcove_perform_0_html5_api')).isPresent()).toBe(true);
 	});
@@ -30,6 +38,30 @@ describe('Video Home Page', function() {
     
 	it('should have the social sharing icons', function() {
 		expect(element(by.className('article_actions inline_sharing clearfix')).isPresent()).toBe(true);
+	});
+    
+	it('should have the recommended section', function() {
+		expect(element(by.className('more_videos row flexslider-container')).isPresent()).toBe(true);
+	});
+    
+	it('should have the featured section', function() {
+		expect(element(by.className('clearfix rule row')).isPresent()).toBe(true);
+	});
+    
+	it('should have the most-popular section', function() {
+		expect(element(by.className('most_popular rule row load_more_container')).isPresent()).toBe(true);
+	});
+    
+	it('should have the latest-video section', function() {
+		expect(element(by.className('latest_video col-sm-8 stream row load_more_container')).isPresent()).toBe(true);
+	});
+    
+	it('should have the video search section', function() {
+		expect(element(by.className('search video_search_control clearfix')).isPresent()).toBe(true);
+	});
+    
+	it('should have the featured-playlists section', function() {
+		expect(element(by.className('featured_playlists rule')).isPresent()).toBe(true);
 	});
     
 });
