83bcffd814310122e1d0227505e69c340be1705c
Kaushal Shah <kshah@forbes.com>
2017-04-20 18:18:02 -0400

    added header and footer checks for csf page

--- test/protractor-spec/csf/pagecheck.spec.js
+++ test/protractor-spec/csf/pagecheck.spec.js
@@ -1,9 +1,18 @@
 var CSFPage = require('./csf.page.js'),
 	csfPage = new CSFPage();
 describe('CSF:', function() {
+    
 	it('should get the page', function() {
 		csfPage.get();
 	});
+    
+    it('should have the header on the page', function() {
+        expect(element(by.tagName('header')).isPresent()).toBe(true); 
+    });
+    
+    it('should have the footer on the page', function() {
+        expect(element(by.tagName('footer')).isPresent()).toBe(true); 
+    });
 
 	globals.generalCheck();
 
