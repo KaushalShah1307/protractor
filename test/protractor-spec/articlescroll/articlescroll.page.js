d2610e8cedc6b6aa84b1fe6fa23f10516cf888e3
Kaushal Shah <kshah@forbes.com>
2017-04-25 18:55:02 -0400

    new suite for article scroll

---
+++ test/protractor-spec/articlescroll/articlescroll.page.js
@@ -0,0 +1,22 @@
+var ArticlePageScroll = function(url) {
+	that = this;
+	that.url = url;
+	that.adsService = 'ArticleAdsService';
+	that.get = function() {
+		browser.get(that.url, 5000);
+		browser.executeAsyncScript(function() {
+			callback = arguments[0];
+			callback($('body').injector().get('ArticleAppState').page_data.articles.article_0.article);
+		}).then(function(result) {
+			that.page_data = result;
+		}).then(function() {
+			browser.current_page = that;
+		});
+		browser.getCurrentUrl().then(function(url) {
+			browser.current_url = url;
+			globals.pagesChecked.push(url);
+		});
+	};
+	return that;
+};
+module.exports = ArticlePageScroll;
---
+++ test/protractor-spec/articlescroll/pagecheck.spec.js
@@ -0,0 +1,56 @@
+var ArticlePageScroll = require('./articlescroll.page.js'),
+	articlePageScroll = new ArticlePageScroll('/sites/davidshaywitz/2017/04/25/three-key-investment-challenges-at-the-interface-of-health-and-technology/?view=prod');
+
+describe('Article:', function() {
+
+	it('should get the page', function() {
+		articlePageScroll.get();
+	});
+    
+    it('should have the title of the article', function() {
+        expect(element.all(by.className('article-headline')).first().getText()).toEqual('Three Key Investment Challenges At The Intersection Of Health And Technology'); 
+    });
+
+    it('should have page views on the article', function(){
+        expect(element.all(by.className('view-count')).first().getText()).toBeGreaterThan('1,564'); 
+    });
+    
+    it('should have the eye icon next to page views', function() {
+        expect(element.all(by.css('.icon.icon-preview-eye')).first().isDisplayed()).toBeTruthy(); 
+    });
+    
+    it('should have the circ link', function() {
+        expect(element(by.className('circ-link'))).toBeTruthy(); 
+    });
+    
+    it('should have the body content', function() {
+        expect(element(by.className('article-injected-body')).length > 0); 
+    });
+    
+    it('should have the tweet quotes module', function() {
+        expect(element(by.className('tweet-this'))).toBeTruthy(); 
+    });
+    
+    it('should not have the Forbes Staff icon next to the author type', function() {
+        expect(element(by.className('icon icon-staff-verified')).isPresent()).toBe(false); 
+    });
+    
+	globals.generalCheck();
+	globals.checkAds(articlePageScroll.adsService);
+});
+ 
+        
+describe('Article Page Scroll:', function() {
+
+    it('should scroll to the next article', function() {
+        browser.executeScript('window.scrollTo(0,document.body.scrollHeight)').then(function(){
+        expect(browser.getCurrentUrl()).not.toEqual(browser.current_page.page_data.uri);
+        }); 
+    });
+    
+    it('should have the title of the 2nd article in the stream', function() {
+        var firstArticleTitle = 'Inside Forbes: The Unstoppable Force That Will Change The News Industry Forever';
+        expect(element.all(by.className('article-headline')).getText()).toEqual('Inside Forbes: The Unstoppable Force That Will Change The News Industry Forever');
+    });
+
+});
