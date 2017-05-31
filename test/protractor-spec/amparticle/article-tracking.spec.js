ce87c0123a00e31b4afd6d19c6e53dcd94454806
Kaushal Shah <kshah@forbes.com>
2017-05-31 15:29:15 -0400

    added checks for SR and GA for AMP article pages

--- test/protractor-spec/amparticle/article-tracking.spec.js
+++ test/protractor-spec/amparticle/article-tracking.spec.js
@@ -32,10 +32,47 @@ describe('AMP Article:', function() {
 */
 		describe('Google Analytics', function() {
             
-			beforeAll(function() {
-			});
-
 			it('should pass the right custom parameters', function() {
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].author')).toEqual('Lewis DVorkin');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].blogType')).toEqual('individual');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].brandVoice')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].brandVoiceLive')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].bvContentSource')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].bvLeftRailHeadline')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].bvProgramType')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].categories')).toEqual('Business,Entrepreneurs,Leadership,Management,Media & Entertainment,Social Media,Tech');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].channel')).toEqual('business');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].contribActive')).toEqual('false');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].contribDivision')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].contribType')).toEqual('Forbes Staff');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].dfpSite')).toEqual('fdcmobile');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].dfpZone')).toEqual('amp/article');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].editSlot')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].forbesOnTrump')).toEqual('false');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].hashtags')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].hashtagsTrending')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].heroImage')).toEqual('false');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].imageCount')).toEqual('3');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].leftRail')).toEqual('false');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].login')).toEqual('false');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].naturalID')).toEqual('blogAndPostId/blog/post/50-13891');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].pageTitle')).toEqual('Inside Forbes: The Unstoppable Force That Will Change The News Industry Forever');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].pageType')).toEqual('GoogleAMP');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].pagination')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].primaryChannel')).toEqual('Business');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].primarySection')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].publishDate')).toEqual('2015-06-10');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].section')).toEqual('lewisdvorkinblog');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].site')).toEqual('lewisdvorkin');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].slot')).toEqual('none');
+                expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[13].innerText))[0].videoPlacement')).toEqual('none');
+                
+			});
+		});
+        
+        describe('SimpleReach', function() {
+            
+            it('should pass the right custom parameters', function() {
                 expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[15].innerText))[0].authors[0]')).toEqual('Lewis DVorkin');
                 expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[15].innerText))[0].categories[0]')).toEqual('business');
                 expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[15].innerText))[0].pid')).toEqual('50e4a8434240cf5c4b000009');
@@ -44,34 +81,10 @@ describe('AMP Article:', function() {
                 expect(browser.executeScript('return window.Object.values(JSON.parse(window.document.scripts[15].innerText))[0].tags.length')).toBe(13);
                 
 			});
-		});
- /*       
-        describe('SimpleReach', function() {
-            var reachpixel;
-            
-            beforeAll(function(done) {
-                reachpixel = $('script#simplereach-script[src*="d8rk54i4mohrb.cloudfront.net"]');
-                reachpixel.getAttribute('src').then(function(src) {
-                    reachpixel.srcString = src;
-                    done();
-                });      
-            });
-            
-            it('should load the SimpleReach script', function() {
-                expect(reachpixel.length > 1);
-            });
-            
-            it('should pass the right custom parameters', function() {
-                expect(browser.executeScript('return window.__reach_config.authors;')).toEqual('Lewis DVorkin'); 
-                expect(browser.executeScript('return window.__reach_config.channels;')).toEqual('business'); 
-                expect(browser.executeScript('return window.__reach_config.date;')).toEqual('2015-06-10T14:00:00.000Z'); 
-                expect(browser.executeScript('return window.__reach_config.pid;')).toEqual('50e4a8434240cf5c4b000009'); 
-                expect(browser.executeScript('return window.__reach_config.title;')).toEqual('Inside Forbes: The Unstoppable Force That Will Change The News Industry Forever'); 
-            });
             
         });
         
-        describe('Comscore', function() {
+        xdescribe('Comscore', function() {
             var comscorepixel;
             
             beforeAll(function(done) {
@@ -86,6 +99,6 @@ describe('AMP Article:', function() {
                 expect(comscorepixel.length > 1);
             });
         });
-*/        
+        
 	});
 });
