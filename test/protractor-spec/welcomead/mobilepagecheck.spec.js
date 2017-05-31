0a521e510ef95cca9d0164c3da53e652f608e7dd
Kaushal Shah <kshah@forbes.com>
2017-05-31 13:11:51 -0400

    added check for GA for both Mobile and Desktop WA

--- test/protractor-spec/welcomead/mobilepagecheck.spec.js
+++ test/protractor-spec/welcomead/mobilepagecheck.spec.js
@@ -35,48 +35,14 @@ describe('Welcome Ad:', function() {
         expect(element(by.css('.body-content>img')).getAttribute('src')).toEqual('https://specials-images.forbesimg.com/imageserve/57e197ac31358e16c589c0b5/40x0.png'); 
     });
     
-    xdescribe('Tracking:', function() {
+    describe('Tracking:', function() {
        
         describe('Google Analytics:', function() {
            
             it('should pass the right custom parameters', function() {
-                expect(browser.executeScript('return window.dataLayer[0].BrandVoiceLive;')).toEqual('false'); 
                 expect(browser.executeScript('return window.dataLayer[0].DFPSite;')).toEqual('fdcmobile'); 
-                expect(browser.executeScript('return window.dataLayer[0].DFPZone;')).toEqual('pictures'); 
-                expect(browser.executeScript('return window.dataLayer[0].author;')).toEqual('Kurt Badenhausen'); 
-                expect(browser.executeScript('return window.dataLayer[0].blogType;')).toEqual('individual'); 
-                expect(browser.executeScript('return window.dataLayer[0].brandVoice;')).toEqual('none'); 
-                //expect(browser.executeScript('return window.dataLayer[0].categories;')).toEqual("[ 'Business', 'Lifestyle', 'Lists', 'Sports & Leisure', 'SportsMoney' ]"); 
-                expect(browser.executeScript('return window.dataLayer[0].channel;')).toEqual('business'); 
-                expect(browser.executeScript('return window.dataLayer[0].contribActive;')).toEqual('false'); 
-                expect(browser.executeScript('return window.dataLayer[0].contribType;')).toEqual('Forbes Staff'); 
-                expect(browser.executeScript('return window.dataLayer[0].customPage;')).toEqual('//www.forbes.com/pictures/mli45fdllh/1-maria-sharapova/'); 
-                expect(browser.executeScript('return window.dataLayer[0].edit;')).toEqual('forbeslife'); 
-                expect(browser.executeScript('return window.dataLayer[0].naturalID;')).toEqual('blogAndSlideId/blog/slide/985-20884'); 
-                expect(browser.executeScript('return window.dataLayer[0].pageNumber;')).toEqual('2'); 
-                expect(browser.executeScript('return window.dataLayer[0].pageTotal;')).toEqual('11'); 
-                expect(browser.executeScript('return window.dataLayer[0].pageType;')).toEqual('slide'); 
-                expect(browser.executeScript('return window.dataLayer[0].primaryChannel;')).toEqual('Business'); 
-                expect(browser.executeScript('return window.dataLayer[0].primarySection;')).toEqual('none'); 
-                expect(browser.executeScript('return window.dataLayer[0].published;')).toEqual('2015-8-12'); 
-                expect(browser.executeScript('return window.dataLayer[0].section;')).toEqual('kurtbadenhausenblog'); 
-                expect(browser.executeScript('return window.dataLayer[0].site;')).toEqual('kurtbadenhausen'); 
-                expect(browser.executeScript('return window.dataLayer[0].slot;')).toEqual('none'); 
-            });
-            
-        });
-        
-        describe('Simple Reach:', function() {
-           
-            it('should pass the right custom parameters', function() {
-                expect(browser.executeScript('return window.__reach_config.authors[0];')).toEqual('Kurt Badenhausen'); 
-                expect(browser.executeScript('return window.__reach_config.channels[0];')).toEqual('business'); 
-                expect(browser.executeScript('return window.__reach_config.date;')).toEqual('2015-08-12T14:00:00Z'); 
-                expect(browser.executeScript('return window.__reach_config.pid;')).toEqual('50e4a8434240cf5c4b000009'); 
-                expect(browser.executeScript('return window.__reach_config.title;')).toEqual("The World's Highest-Paid Female Athletes 2015 -Gallery"); 
-                expect(browser.executeScript('return window.__reach_config.tags[0];')).toEqual('site::kurtbadenhausen'); 
-                expect(browser.executeScript('return window.__reach_config.tags[1];')).toEqual('slot::'); 
-                expect(browser.executeScript('return window.__reach_config.tags[2];')).toEqual('type::slide'); 
+                expect(browser.executeScript('return window.dataLayer[0].DFPZone;')).toEqual('welcome'); 
+                expect(browser.executeScript('return window.dataLayer[0].channel;')).toEqual('ads'); 
             });
             
         });
