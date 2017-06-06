e5eb95c225ee18211088137f08cc74ae0ed58556
Kaushal Shah <kshah@forbes.com>
2017-06-06 13:23:56 -0400

    NG2 swimlaned article with nostream-one/two should not have the medianet and revcontent units

--- test/protractor-spec/ng2articlechecks/pagecheck.spec.js
+++ test/protractor-spec/ng2articlechecks/pagecheck.spec.js
@@ -149,7 +149,7 @@ describe('NG2 Article Checks:', function() {
     describe('Swimlane=NoStream-one/two Article:', function() {
         
         it('should get the page', function() {
-            var url = 'https://www.forbes.com/sites/lewisdvorkin/2015/06/10/inside-forbes-the-unstoppable-force-that-will-change-the-news-industry-forever/?ss=nostream-one';
+            var url = 'https://www.forbes.com/sites/lewisdvorkin/2015/06/10/inside-forbes-the-unstoppable-force-that-will-change-the-news-industry-forever/?ss=nostream-one&view=beta-u';
             browser.get(url);
             globals.pagesChecked.push(url);
         });
@@ -161,6 +161,18 @@ describe('NG2 Article Checks:', function() {
         
         it('should have the sig file', function() {
             expect(element(by.tagName('sig-file')).isPresent()).toBe(true); 
+        });
+    
+        it('should not have the medianet unit', function() {
+            var mnetUnit = element(by.id('_mN_dy_289199738'));
+            expect(browser.isElementPresent(mnetUnit)).toBe(false);     
+        });
+
+        it('should not have the revcontent unit', function() {
+            //expect(element.all(by.className('rc-w-30022 rc-p rc-p-pt')).first().isPresent()).toBe(true); 
+            //expect(element.all(by.className('rc-w-30022 rc-p rc-p-pt')).first().isDisplayed()).toBe(true);
+            var revContent = element.all(by.className('rc-w-30022 rc-p rc-p-pt')).first();
+            expect(browser.isElementPresent(revContent)).toBe(false);
         });
     });  
     
