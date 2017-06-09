44b3f55ccd667808d63182cf71043db899f8269a
Kaushal Shah <kshah@forbes.com>
2017-06-09 15:24:17 -0400

    added a new sub suite for NG2 articles with noleftrailview that is for smaller breakpoints

---
+++ test/protractor-spec/ng2article/noleftrailads.spec.js
@@ -0,0 +1,69 @@
+describe('NG2 Article Ads on Smaller Breakpoints:', function() {
+   
+    it('should have the medianet unit', function() {
+        expect(element(by.id('_mN_dy_289199738')).isPresent()).toBe(true);      
+    });
+    
+    it('should have the revcontent unit', function() {
+        expect(element.all(by.className('rc-w-30022 rc-p rc-p-pt')).first().isPresent()).toBe(true); 
+    });
+    
+    it('should have all the ads', function() {
+        expect(browser.executeScript('return window.Object.keys(external_services.ad_slots).length')).toBeGreaterThanOrEqual(8); 
+    });
+    
+    it('should have ntv-belt ad', function() {
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ntv-belt"])[0]')).not.toBeNull(); 
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ntv-belt"])[1]')).not.toBeNull(); 
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ntv-belt"])[2]')).toBe(2); 
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ntv-belt"])[3]')).toBe(3); 
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ntv-belt"])[4]')).toEqual('ntv-belt');
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[1].getSizes().length')).toBe(2);
+    });
+    
+    it('should have spon-logo ad', function() {
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-spon-logo"])[4]')).toEqual('ad-rail-0-spon-logo');
+    });
+    
+    it('should have top ad', function() {
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["article-0-top"])[4]')).toEqual('article-0-top');
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["article-0-top"])[2]')).toBeGreaterThanOrEqual(1);
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["article-0-top"])[3]')).toBeGreaterThanOrEqual(1);
+    });
+    
+    it('should have loge ad', function() {
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-loge"])[4]')).toEqual('ad-rail-0-loge');
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-loge"])[2]')).toBeGreaterThanOrEqual(1);
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-loge"])[3]')).toBeGreaterThanOrEqual(1);
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[3].getSizes().length')).toBe(7);
+    });
+    
+    it('should have rec ad', function() {
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-rec"])[4]')).toEqual('ad-rail-0-rec');
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-rec"])[2]')).toBeGreaterThanOrEqual(1);
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-rec"])[3]')).toBeGreaterThanOrEqual(1);
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[4].getSizes().length')).toBe(7);
+    });
+    
+    it('should have recx-1 ad', function() {
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-recx-1"])[4]')).toEqual('ad-rail-0-recx-1');
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-recx-1"])[2]')).toBeGreaterThanOrEqual(1);
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-recx-1"])[3]')).toBeGreaterThanOrEqual(1);
+    });
+    
+    it('should have recx-2 ad', function() {
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-recx-2"])[4]')).toEqual('ad-rail-0-recx-2');
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-recx-2"])[2]')).toBeGreaterThanOrEqual(1);
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["ad-rail-0-recx-2"])[3]')).toBeGreaterThanOrEqual(1);
+    });
+    
+    it('should have inread ad', function() {
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["inread"])[4]')).toEqual('inread');
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["inread"])[2]')).toBeGreaterThanOrEqual(1);
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["inread"])[3]')).toBeGreaterThanOrEqual(1);
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["inread"])[0]')).not.toBeUndefined();
+        expect(browser.executeScript('return window.Object.values(external_services.ad_slots["inread"])[1]')).not.toBeUndefined();
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getSizes().length')).toBe(3);
+    });
+ 
+});
---
+++ test/protractor-spec/ng2article/noleftrailview.spec.js
@@ -0,0 +1,42 @@
+describe('NG2 Article on Smaller Breakpoint:', function() {
+	var currentUrl;
+
+	beforeAll(function(done) {
+		browser.getCurrentUrl().then(function(url) {
+			currentUrl = url;
+			done();
+		});
+	});
+    
+    describe('Vestpockets:', function() {
+
+        it('should have vestpockets', function() {
+            var vestpocket = element(by.css('.vestpocket'));
+            expect(vestpocket.isPresent()).toBe(true);
+            expect(element(by.css('.slick-initialized.slick-slider')).isPresent()).toBe(true);
+        });
+
+        it('should not have brandvoice treatment', function() {
+            var brandVoiceStyling = element(by.css('.brandvoice'));
+            expect(browser.isElementPresent(brandVoiceStyling)).toBe(false);
+        });
+    
+    });
+    
+    describe('Trending Drawer:', function() {
+        var trendingDrawer = element(by.css('.trending'));
+
+        it('should have trending drawer', function() {
+            expect(trendingDrawer.isPresent()).toBe(true);
+        });
+
+        it('should click to expand trending drawer', function() {
+            trendingDrawer.click();
+            expect(element(by.css('.siderail-container')).isPresent()).toBe(true);
+            var articleUrl = '/sites/lewisdvorkin/2015/06/10/inside-forbes-the-unstoppable-force-that-will-change-the-news-industry-forever/';
+            expect(element.all(by.css('.current-inline>inline-article>a')).first().getAttribute('href')).toContain(articleUrl);
+            trendingDrawer.click();
+        });
+    
+    });
+});
