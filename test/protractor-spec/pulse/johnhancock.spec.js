cf2c7837e5d8c49cc2f6c1f8ea9888210f7e3802
Kaushal Shah <kshah@forbes.com>
2017-07-12 13:20:02 -0400

    added check for John Hancock Pulse

---
+++ test/protractor-spec/pulse/johnhancock.spec.js
@@ -0,0 +1,128 @@
+var Pulse = require('./pulse.page.js'),
+	pulse = new Pulse();
+
+describe('Pulse - John Hancock:', function() {
+    
+	it('should get the page', function() {
+        var url = 'https://www.forbes.com/johnhancock/in-a-lifetime-healthcare/';
+        browser.get(url);
+        globals.pagesChecked.push(url);
+    });
+    
+    it('should have the header on the page', function() {
+        expect(element(by.tagName('header')).isPresent()).toBe(true); 
+    });
+    
+    it('should have the footer on the page', function() {
+        expect(element(by.tagName('footer')).isPresent()).toBe(true); 
+    });
+    
+    it('should have brandvoice blurb', function() {
+        expect(element(by.css('.advoice-desc.initialized.ng-scope.ng-isolate-scope')).isDisplayed()).toBe(true); 
+    });
+    
+    globals.checkAds(pulse.adsService);
+});
+
+describe('Pulse - John Hancock:', function() {
+	var currentUrl;
+
+	beforeAll(function(done) {
+		browser.getCurrentUrl().then(function(url) {
+			currentUrl = url;
+			done();
+		});
+	});
+
+	describe('Tracking on Pulse:', function() {
+		describe('Fast Pixel', function() {
+			var trackingPixel;
+
+			beforeAll(function(done) {
+				trackingPixel = $('img[src*="fast.forbes.com"]');
+				trackingPixel.getAttribute('src').then(function(src) {
+					trackingPixel.srcString = src;
+					done();
+				});
+			});
+
+			it ('should have the correct parameters', function() {
+				expect(globals.getParam(trackingPixel.srcString, 'su')).toContain('http://www.forbes.com/dell/cities-transformed/');
+				expect(globals.getParam(trackingPixel.srcString, 'au')).toEqual('blogAuthorId/blog/author/1795913');
+				expect(globals.getParam(trackingPixel.srcString, 'trl')).not.toBeNull();
+				expect(globals.getParam(trackingPixel.srcString, 'se')).toEqual('dell - cities transformed');
+				expect(globals.getParam(trackingPixel.srcString, 'ch')).toEqual('tech');
+			});
+		});
+        
+		describe('Google Analytics', function() {
+			var dataLayer;
+			beforeAll(function() {
+				browser.executeScript(function() {
+					return dataLayer[0];
+				}).then(function(result) {
+					dataLayer = result;
+				});
+			});
+
+			it('should pass the right custom parameters', function() {
+                var sites = ['fdcmobile', 'fdc.forbes'];
+                expect(sites).toContain(dataLayer.DFPSite);
+                expect(dataLayer.DFPZone).toEqual('csf');
+				expect(dataLayer.channel).toEqual('tech');
+                expect(dataLayer.section).toEqual('dell - cities transformed');
+                expect(dataLayer.brandVoice).toEqual('delltechnologies');
+                expect(dataLayer.brandVoiceLive).toEqual('false');
+                expect(dataLayer.pageType).toEqual('csf');
+                expect(dataLayer.primaryChannel).toEqual('Business');
+                expect(dataLayer.primarySection).toEqual('Dell - Cities Transformed');
+				expect(dataLayer.slot).toEqual('dellsfcities');
+			});
+		});
+        
+        describe('SimpleReach', function() {
+            var reachpixel;
+            
+            beforeAll(function(done) {
+                reachpixel = $('script#simplereach-script[src*="d8rk54i4mohrb.cloudfront.net"]');
+                reachpixel.getAttribute('src').then(function(src) {
+                    reachpixel.srcString = src;
+                    done();
+                });      
+            });
+            
+            it('should load the SimpleReach script', function() {
+                expect(reachpixel.length > 1);
+            });
+            
+            it('should pass the right custom parameters', function() {
+                expect(browser.executeScript('return window.__reach_config.date;')).toEqual('2016-06-16T20:15:47.179Z'); 
+                expect(browser.executeScript('return window.__reach_config.pid;')).toEqual('50e4a8434240cf5c4b000009'); 
+                expect(browser.executeScript('return window.__reach_config.title;')).toEqual('Dell - Cities Transformed'); 
+                expect(browser.executeScript('return window.__reach_config.tags[0];')).toEqual('brandvoice'); 
+                expect(browser.executeScript('return window.__reach_config.tags[1];')).toEqual('site::delltechnologies'); 
+                expect(browser.executeScript('return window.__reach_config.tags[2];')).toEqual('slot::dellsfcities'); 
+                expect(browser.executeScript('return window.__reach_config.tags[3];')).toEqual('type::special feature'); 
+                expect(browser.executeScript('return window.__reach_config.channels[0];')).toEqual('tech'); 
+            });
+            
+        });
+        
+        describe('Comscore', function() {
+            var comscorepixel;
+            
+            beforeAll(function(done) {
+                comscorepixel = $('script[src*="b.scorecardresearch.com/beacon.js"]');
+                comscorepixel.getAttribute('src').then(function(src) {
+                    comscorepixel.srcString = src;
+                    done();
+                });      
+            });
+            
+            it('should load the ComScore script', function() {
+                expect(comscorepixel.length > 1);
+            });
+        });        
+        
+	});        
+});
