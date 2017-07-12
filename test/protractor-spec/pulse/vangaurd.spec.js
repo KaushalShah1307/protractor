07c99722b5529e7d8cac46b574271ff1b5786f16
Kaushal Shah <kshah@forbes.com>
2017-07-12 16:47:29 -0400

    added checking and tracking for Vanguard Pulse

--- test/protractor-spec/pulse/vangaurd.spec.js
+++ test/protractor-spec/pulse/vangaurd.spec.js
@@ -1,10 +1,10 @@
 var Pulse = require('./pulse.page.js'),
 	pulse = new Pulse();
 
-describe('Pulse - United:', function() {
+describe('Pulse - Vanguard:', function() {
     
 	it('should get the page', function() {
-        var url = 'https://www.forbes.com/united/street-to-street/';
+        var url = 'https://www.forbes.com/vanguard/destination-retirement/';
         browser.get(url);
         globals.pagesChecked.push(url);
     });
@@ -21,7 +21,7 @@ describe('Pulse - United:', function() {
         expect(element(by.css('.advoice-desc.initialized.ng-scope.ng-isolate-scope')).isDisplayed()).toBe(true); 
     });
 
-describe('Pulse - United:', function() {
+describe('Pulse - Vanguard:', function() {
 	var currentUrl;
 
 	beforeAll(function(done) {
@@ -44,8 +44,8 @@ describe('Pulse - United:', function() {
 			});
 
 			it ('should have the correct parameters', function() {
-				expect(globals.getParam(trackingPixel.srcString, 'su')).toContain('http://www.forbes.com/united/street-to-street/');
-				expect(globals.getParam(trackingPixel.srcString, 'au')).toEqual('blogAuthorId/blog/author/985544');
+				expect(globals.getParam(trackingPixel.srcString, 'su')).toContain('http://www.forbes.com/vanguard/destination-retirement/');
+				expect(globals.getParam(trackingPixel.srcString, 'au')).toEqual('blogAuthorId/blog/author/2938290');
 				expect(globals.getParam(trackingPixel.srcString, 'trl')).not.toBeNull();
 			});
 		});
@@ -66,13 +66,13 @@ describe('Pulse - United:', function() {
                 expect(dataLayer.DFPZone).toEqual('csf');
 				expect(dataLayer.channel).toEqual('none');
                 expect(dataLayer.section).toEqual('none');
-                expect(dataLayer.brandVoice).toEqual('united');
+                expect(dataLayer.brandVoice).toEqual('vanguard');
                 expect(dataLayer.bvProgramType).toEqual('special_feature');
                 expect(dataLayer.brandVoiceLive).toEqual('true');
                 expect(dataLayer.pageType).toEqual('csf');
                 expect(dataLayer.primaryChannel).toEqual('none');
-                expect(dataLayer.primarySection).toEqual('Street to Street');
-				expect(dataLayer.slot).toEqual('unitedsf');
+                expect(dataLayer.primarySection).toEqual('Destination Retirement');
+				expect(dataLayer.slot).toEqual('vanguardsf');
 			});
 		});
         
@@ -92,12 +92,12 @@ describe('Pulse - United:', function() {
             });
             
             it('should pass the right custom parameters', function() {
-                expect(browser.executeScript('return window.__reach_config.date;')).toEqual('2017-03-28T17:40:01.266Z'); 
+                expect(browser.executeScript('return window.__reach_config.date;')).toEqual('2017-06-02T15:06:20.660Z'); 
                 expect(browser.executeScript('return window.__reach_config.pid;')).toEqual('50e4a8434240cf5c4b000009'); 
-                expect(browser.executeScript('return window.__reach_config.title;')).toEqual('UnitedVoice: Street To Street'); 
+                expect(browser.executeScript('return window.__reach_config.title;')).toEqual('VanguardVoice: Destination Retirement'); 
                 expect(browser.executeScript('return window.__reach_config.tags[0];')).toEqual('brandvoice'); 
-                expect(browser.executeScript('return window.__reach_config.tags[1];')).toEqual('site::united'); 
-                expect(browser.executeScript('return window.__reach_config.tags[2];')).toEqual('slot::unitedsf'); 
+                expect(browser.executeScript('return window.__reach_config.tags[1];')).toEqual('site::vanguard'); 
+                expect(browser.executeScript('return window.__reach_config.tags[2];')).toEqual('slot::vanguardsf'); 
                 expect(browser.executeScript('return window.__reach_config.tags[3];')).toEqual('type::special feature');
             });
             
