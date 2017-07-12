8458d3ce5243d67f0c84bf3c5e7de48ba8dc4f34
Kaushal Shah <kshah@forbes.com>
2017-07-12 13:24:48 -0400

    fixed the tracking for John Hancock Pulse

--- test/protractor-spec/pulse/johnhancock.spec.js
+++ test/protractor-spec/pulse/johnhancock.spec.js
@@ -47,11 +47,11 @@ describe('Pulse - John Hancock:', function() {
 			});
 
 			it ('should have the correct parameters', function() {
-				expect(globals.getParam(trackingPixel.srcString, 'su')).toContain('http://www.forbes.com/dell/cities-transformed/');
-				expect(globals.getParam(trackingPixel.srcString, 'au')).toEqual('blogAuthorId/blog/author/1795913');
+				expect(globals.getParam(trackingPixel.srcString, 'su')).toContain('http://www.forbes.com/johnhancock/in-a-lifetime-healthcare/');
+				expect(globals.getParam(trackingPixel.srcString, 'au')).toEqual('blogAuthorId/blog/author/780463');
 				expect(globals.getParam(trackingPixel.srcString, 'trl')).not.toBeNull();
-				expect(globals.getParam(trackingPixel.srcString, 'se')).toEqual('dell - cities transformed');
-				expect(globals.getParam(trackingPixel.srcString, 'ch')).toEqual('tech');
+				expect(globals.getParam(trackingPixel.srcString, 'se')).toEqual('in a lifetime');
+				expect(globals.getParam(trackingPixel.srcString, 'ch')).toEqual('business');
 			});
 		});
         
@@ -69,14 +69,14 @@ describe('Pulse - John Hancock:', function() {
                 var sites = ['fdcmobile', 'fdc.forbes'];
                 expect(sites).toContain(dataLayer.DFPSite);
                 expect(dataLayer.DFPZone).toEqual('csf');
-				expect(dataLayer.channel).toEqual('tech');
-                expect(dataLayer.section).toEqual('dell - cities transformed');
-                expect(dataLayer.brandVoice).toEqual('delltechnologies');
+				expect(dataLayer.channel).toEqual('business');
+                expect(dataLayer.section).toEqual('in a lifetime');
+                expect(dataLayer.brandVoice).toEqual('johnhancock');
                 expect(dataLayer.brandVoiceLive).toEqual('false');
                 expect(dataLayer.pageType).toEqual('csf');
                 expect(dataLayer.primaryChannel).toEqual('Business');
-                expect(dataLayer.primarySection).toEqual('Dell - Cities Transformed');
-				expect(dataLayer.slot).toEqual('dellsfcities');
+                expect(dataLayer.primarySection).toEqual('In A Lifetime');
+				expect(dataLayer.slot).toEqual('johnhancocksf-health');
 			});
 		});
         
@@ -96,14 +96,14 @@ describe('Pulse - John Hancock:', function() {
             });
             
             it('should pass the right custom parameters', function() {
-                expect(browser.executeScript('return window.__reach_config.date;')).toEqual('2016-06-16T20:15:47.179Z'); 
+                expect(browser.executeScript('return window.__reach_config.date;')).toEqual('2016-09-16T18:46:48.632Z'); 
                 expect(browser.executeScript('return window.__reach_config.pid;')).toEqual('50e4a8434240cf5c4b000009'); 
-                expect(browser.executeScript('return window.__reach_config.title;')).toEqual('Dell - Cities Transformed'); 
+                expect(browser.executeScript('return window.__reach_config.title;')).toEqual('John HancockVoice: In A Lifetime'); 
                 expect(browser.executeScript('return window.__reach_config.tags[0];')).toEqual('brandvoice'); 
-                expect(browser.executeScript('return window.__reach_config.tags[1];')).toEqual('site::delltechnologies'); 
-                expect(browser.executeScript('return window.__reach_config.tags[2];')).toEqual('slot::dellsfcities'); 
+                expect(browser.executeScript('return window.__reach_config.tags[1];')).toEqual('site::johnhancock'); 
+                expect(browser.executeScript('return window.__reach_config.tags[2];')).toEqual('slot::johnhancocksf-health'); 
                 expect(browser.executeScript('return window.__reach_config.tags[3];')).toEqual('type::special feature'); 
-                expect(browser.executeScript('return window.__reach_config.channels[0];')).toEqual('tech'); 
+                expect(browser.executeScript('return window.__reach_config.channels[0];')).toEqual('business'); 
             });
             
         });
