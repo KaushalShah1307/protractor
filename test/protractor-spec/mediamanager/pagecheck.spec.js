5c825d33833f5a448e27bdfdc2621388971bda08
Kaushal Shah <kshah@forbes.com>
2017-07-18 17:48:12 -0400

    added a check for header links for media manager

--- test/protractor-spec/mediamanager/pagecheck.spec.js
+++ test/protractor-spec/mediamanager/pagecheck.spec.js
@@ -7,17 +7,35 @@ describe('Media Manager:', function() {
         mediamanager.get();
     });
     
-    it('should have login module', function() {
-        expect(element(by.css('.login-wrapper.ng-scope')).isDisplayed()).toBe(true); 
+    describe('login:', function() {
+    
+        it('should have login module', function() {
+            expect(element(by.css('.login-wrapper.ng-scope')).isDisplayed()).toBe(true); 
+        });
+
+        it('should login', function() {
+            element(by.model('username')).sendKeys('testguy');
+            element(by.model('password')).sendKeys('Forbes123');
+            element(by.css('.button')).click();
+        });
     });
     
-    it('should login', function() {
-        element(by.model('username')).sendKeys('testguy');
-        element(by.model('password')).sendKeys('Forbes123');
-        element(by.css('.button')).click();
-    });
+    describe('Header:', function() {
     
-    it('should have header logo', function() {
-        expect(element.all(by.css('.forbes-logo>a')).get(1).getText()).toEqual('Media Manager');
-    });
+        it('should have header logo', function() {
+            expect(element.all(by.css('.forbes-logo>a')).get(1).getText()).toEqual('Media Manager');
+        });
+
+        it('should have Create Gallery link', function() {
+            expect(element.all(by.css('.dam-links>ul>li>a')).get(0).getText()).toEqual('CREATE GALLERY');
+        });
+
+        it('should have My Images link', function() {
+            expect(element.all(by.css('.dam-links>ul>li>a')).get(1).getText()).toEqual('MY IMAGES');
+        });
+
+        it('should have Log Out link', function() {
+            expect(element.all(by.css('.dam-links>ul>li>a')).get(2).getText()).toEqual('LOG OUT');
+        });
+    });    
 });
