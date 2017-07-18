d74b25ed7992ab9acfe11b6ff861505d9110f93b
Kaushal Shah <kshah@forbes.com>
2017-07-18 17:37:25 -0400

    added a check to login and check for the header logo for mediamanager

--- test/protractor-spec/mediamanager/mediamanager.page.js
+++ test/protractor-spec/mediamanager/mediamanager.page.js
@@ -1,7 +1,6 @@
 var MediaManager = function() {
   this.get = function() {
     browser.get('/media-manager/#/');
-    browser.ignoreSynchronization = true;
     browser.getCurrentUrl().then(function(url) {
 		browser.current_url = url;
         globals.pagesChecked.push(url);
--- test/protractor-spec/mediamanager/pagecheck.spec.js
+++ test/protractor-spec/mediamanager/pagecheck.spec.js
@@ -5,5 +5,19 @@ describe('Media Manager:', function() {
     
 	it('should get the page', function() {
         mediamanager.get();
-    });   
+    });
+    
+    it('should have login module', function() {
+        expect(element(by.css('.login-wrapper.ng-scope')).isDisplayed()).toBe(true); 
+    });
+    
+    it('should login', function() {
+        element(by.model('username')).sendKeys('testguy');
+        element(by.model('password')).sendKeys('Forbes123');
+        element(by.css('.button')).click();
+    });
+    
+    it('should have header logo', function() {
+        expect(element.all(by.css('.forbes-logo>a')).get(1).getText()).toEqual('Media Manager');
+    });
 });
