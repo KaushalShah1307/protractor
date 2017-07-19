a9c53affac968763d18ab03b511e70475a6fc75e
Kaushal Shah <kshah@forbes.com>
2017-07-19 14:29:48 -0400

    declared resused variables globally, added a check for all the sources image search

--- test/protractor-spec/mediamanager/pagesearch.spec.js
+++ test/protractor-spec/mediamanager/pagesearch.spec.js
@@ -1,4 +1,8 @@
 describe('Image Search:', function() {
+    
+    var sources = element.all(by.css('.radio-wrapper>label'));
+    var search = element(by.css('.icon.icon-search'));
+    var results = element(by.css('.section-title.num-of-results'));
    
     it('should get the page', function() {
         element.all(by.css('.forbes-logo>a')).get(1).click(); 
@@ -11,14 +15,34 @@ describe('Image Search:', function() {
     it('should have image sources', function() {
         var imageSources = ['Getty', 'AP', 'Bloomberg', 'Shutterstock', 'Forbes'];
             for (i=0; i<5; i++) {
-                expect(imageSources).toContain(element.all(by.css('.radio-wrapper>label')).get(i).getText());  
+                expect(imageSources).toContain(sources.get(i).getText());  
             }; 
     });
     
-    it('should search for an image', function() {
+    it('should search for Getty images', function() {
         var searchBar = element(by.model('search_data.query'));
         searchBar.click(); 
-        searchBar.sendKeys('Forbes/');
+        searchBar.sendKeys('Forbes');
+        search.click();
+        expect(results.getText()).toContain('images for “Forbes”');
+    });
+    
+    it('should search for AP images', function() {
+        sources.get(1).click();
+        search.click();
+        expect(results.getText()).toContain('images for “Forbes”');
+    });
+    
+    it('should search for Bloomberg images', function() {
+        sources.get(2).click();
+        search.click();
+        expect(results.getText()).toContain('images for “Forbes”');
+    });
+    
+    it('should search for Shutterstock images', function() {
+        sources.get(3).click();
+        search.click();
+        expect(results.getText()).toContain('images for “Forbes”');
     });
     
 });
