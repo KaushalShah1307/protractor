2f3ca065ee07c4e3a98725fb3d9a76dbf8919d45
Kaushal Shah <kshah@forbes.com>
2017-07-24 13:26:55 -0400

    added more timeout for amp to return results for articles only

--- test/protractor-spec/amparticle/validation.spec.js
+++ test/protractor-spec/amparticle/validation.spec.js
@@ -1,13 +1,13 @@
 describe('AMP Article Validation:', function() {
    
     it('should load AMP Validator site', function() {
-        var url = 'https://validator.ampproject.org/#url=https%3A%2F%2Fwww.forbes.com%2Fsites%2Flewisdvorkin%2F2015%2F06%2F10%2Finside-forbes-the-unstoppable-force-that-will-change-the-news-industry-forever%2Famp%2F%23development%3D1';
+        var url = 'https://validator.ampproject.org/#url=https%3A%2F%2Fwww.forbes.com%2Fsites%2Flewisdvorkin%2F2015%2F06%2F10%2Finside-forbes-the-unstoppable-force-that-will-change-the-news-industry-forever%2Famp%2F';
         browser.get(url);
         globals.pagesChecked.push(url);
     });
     
     it('should check for validation', function() {
-        browser.sleep(2000);
+        browser.sleep(3000);
         expect(element(by.css('.ampproject-result.style-scope.webui-statusbar')).getText()).not.toContain('FAIL');
     });
     
