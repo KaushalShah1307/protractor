81e3b81f87b9c571e128000fcc9856edb7281e70
Kaushal Shah <kshah@forbes.com>
2017-07-31 15:10:53 -0400

    checking for headline textarea for drafts

--- test/protractor-spec/forbespress/3.draft.spec.js
+++ test/protractor-spec/forbespress/3.draft.spec.js
@@ -1,4 +1,7 @@
 describe('Compose Draft:', function() {
+   var saveButton = element(by.css('.fp-button.button-save'));
+   var publishButton = element(by.css('.fp-button.button-publish'));
+   var headline = element.all(by.css('.title.headline-input')).first();    
     
    it('should click create button', function() {
        var compose = element(by.css('.button-new'));
@@ -7,6 +10,11 @@ describe('Compose Draft:', function() {
     
    it('should have the correct url', function() {
        expect(browser.getCurrentUrl()).toContain('compose');
-   }); 
+   });
+    
+   it('should have headline area', function() {
+       expect(headline.isDisplayed()).toBe(true);
+       expect(headline.getAttribute('placeholder')).toEqual('Add your headline...');
+   });
    
 });
