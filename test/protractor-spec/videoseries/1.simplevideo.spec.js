12b9ce95bfe7fd0c14e186390c1d82e3533a2046
Kaushal Shah <kshah@forbes.com>
2017-08-01 15:33:20 -0400

    added tracking for the simple video homepage and added check for ads on the page

--- test/protractor-spec/videoseries/1.simplevideo.spec.js
+++ test/protractor-spec/videoseries/1.simplevideo.spec.js
@@ -8,8 +8,26 @@ beforeEach(function(){
 
 describe('Simple Video Homepage:', function() {
     
-   it('get the page', function() {
-      simpleVideo.get(); 
-   }); 
+    it('get the page', function() {
+       simpleVideo.get(); 
+    });
+    
+    it('should have the header', function() {
+        expect(element(by.css('.header')).isDisplayed()).toBe(true); 
+    });
+    
+    it('should have the footer', function() {
+        expect(element(by.tagName('footer')).isDisplayed()).toBe(true); 
+    });        
+    
+});
+
+describe('Ads', function() {
+    
+    it('should have top ad', function() {
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('scp=pos%3Dtop'); 
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('iu=7175%2Ffdc.forbes%2Fvideo'); 
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('sz=320x50%7C728x90%7C970x250%7C970x90%7C970x66%7C640x360%7C1x1&fluid=height'); 
+    });
     
 });
---
+++ test/protractor-spec/videoseries/2.tracking-simplevideo.spec.js
@@ -0,0 +1,99 @@
+describe('Simple Video Homepage:', function() {
+	var currentUrl;
+
+	beforeAll(function(done) {
+		browser.getCurrentUrl().then(function(url) {
+			currentUrl = url;
+			done();
+		});
+	});
+
+	describe('Tracking:', function() {
+		xdescribe('Fast Pixel', function() {
+			var trackingPixel;
+
+			beforeAll(function(done) {
+				trackingPixel = $('img[src*="fast.forbes.com"]');
+				trackingPixel.getAttribute('src').then(function(src) {
+					trackingPixel.srcString = src;
+					done();
+				});
+			});
+
+			it('should have the correct parameters', function() {
+				expect(globals.getParam(trackingPixel.srcString, 'su')).toContain('www.forbes.com/sites/lewisdvorkin/2015/06/10/inside-forbes-the-unstoppable-force-that-will-change-the-news-industry-forever/');
+				expect(globals.getParam(trackingPixel.srcString, 'pt')).toEqual(browser.current_page.page_data.type);
+				expect(globals.getParam(trackingPixel.srcString, 'i')).toEqual(browser.current_page.page_data.naturalId);
+				expect(globals.getParam(trackingPixel.srcString, 'at')).toEqual(browser.current_page.page_data.blogType);
+				expect(globals.getParam(trackingPixel.srcString, 'ch')).toEqual(browser.current_page.page_data.displayChannel);
+				expect(globals.getParam(trackingPixel.srcString, 'au')).toEqual(browser.current_page.page_data.authors[0].naturalId);
+			});
+		});
+
+		describe('Google Analytics', function() {
+			var dataLayer;
+			beforeAll(function() {
+				browser.executeScript(function() {
+					return dataLayer[0];
+				}).then(function(result) {
+					dataLayer = result;
+				});
+			});
+
+			it('should pass the right custom parameters', function() {
+                expect(dataLayer.DFPSite).toEqual('fdc.forbes');
+                expect(dataLayer.DFPZone).toEqual('video');
+				expect(dataLayer.channel).toEqual('video');
+				expect(dataLayer.contentTitle).toEqual('TODO Page title');
+				expect(dataLayer.naturalID).toEqual('TODO Page Id');
+				expect(dataLayer.primaryChannel).toEqual('video');
+				expect(dataLayer.displayedChannel).toEqual('video');
+                expect(dataLayer.pageType).toEqual('video');
+			});
+		});
+        
+        xdescribe('SimpleReach', function() {
+            
+            it('should pass the right custom parameters', function() {
+                expect(browser.executeScript('return window.__reach_config.authors;')).toEqual('Lewis DVorkin'); 
+                expect(browser.executeScript('return window.__reach_config.channels;')).toEqual('business'); 
+                expect(browser.executeScript('return window.__reach_config.date;')).toEqual('2015-06-10T14:00:00.000Z'); 
+                expect(browser.executeScript('return window.__reach_config.pid;')).toEqual('50e4a8434240cf5c4b000009'); 
+                expect(browser.executeScript('return window.__reach_config.tags.length;')).toEqual(13); 
+                expect(browser.executeScript('return window.__reach_config.title;')).toEqual('Inside Forbes: The Unstoppable Force That Will Change The News Industry Forever'); 
+            });
+            
+        });
+        
+        describe('Comscore', function() {
+            var comscorepixel;
+            
+            beforeAll(function(done) {
+                comscorepixel = $('script[src*="b.scorecardresearch.com/beacon.js"]');
+                comscorepixel.getAttribute('src').then(function(src) {
+                    comscorepixel.srcString = src;
+                    done();
+                });      
+            });
+            
+            it('should load the ComScore script', function() {
+                expect(comscorepixel.length > 1);
+            });
+        });
+        
+        describe('Chartbeat', function() {
+            
+            it('should pass the right custom parameters', function() {
+                expect(browser.executeScript('return window.trackingService.chartbeatService.sfAsyncConfig.authors')).toEqual('');
+                expect(browser.executeScript('return window.trackingService.chartbeatService.sfAsyncConfig.domain')).toEqual('forbes.com');
+                expect(browser.executeScript('return window.trackingService.chartbeatService.sfAsyncConfig.mabServer')).toEqual('mabping.chartbeat.net');
+                expect(browser.executeScript('return window.trackingService.chartbeatService.sfAsyncConfig.path')).toEqual('/video/');
+                expect(browser.executeScript('return window.trackingService.chartbeatService.sfAsyncConfig.pingServer')).toEqual('ping.chartbeat.net');
+                expect(browser.executeScript('return window.trackingService.chartbeatService.sfAsyncConfig.sections')).toEqual('');
+                expect(browser.executeScript('return window.trackingService.chartbeatService.sfAsyncConfig.title')).toEqual('Forbes - The Brooklyn Startup Bringing Eyewear Manufacturing Back To America');
+                expect(browser.executeScript('return window.trackingService.chartbeatService.sfAsyncConfig.uid')).toBe(17493);
+            });
+        });
+        
+	});
+});
