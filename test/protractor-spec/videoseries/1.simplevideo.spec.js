355637d422db0c7c896a8a61605189eb369f970b
Kaushal Shah <kshah@forbes.com>
2017-08-07 18:35:39 -0400

    check for 30 videos in the latest video section

--- test/protractor-spec/videoseries/1.simplevideo.spec.js
+++ test/protractor-spec/videoseries/1.simplevideo.spec.js
@@ -27,37 +27,14 @@ describe('Simple Video Homepage:', function() {
     });
     
     it('should have latest video section', function() {
-        expect(sections.get(1).isDisplayed()).toBe(true); 
+        expect(sections.get(1).isDisplayed()).toBe(true);
+        expect(element(by.css('.fbs-slider.fbs-slider--multiple.fbs-slider--latest')).getAttribute('data-in-view')).toEqual('3');
+        expect(element.all(by.css('.grid__index')).count()).toBeGreaterThanOrEqual(30);
     });
     
     it('should have recommended video section', function() {
-        expect(sections.get(2).isDisplayed()).toBe(true); 
+        expect(sections.get(2).isDisplayed()).toBe(true);
+        expect(element(by.css('.fbs-slider.fbs-slider--multiple.fbs-slider--recommended')).getAttribute('data-in-view')).toEqual('4'); 
     });
-    
-});
-
-describe('Ads', function() {
-    
-    var isMobile = browser.executeScript("return window.matchMedia('only screen and (max-width: 760px)').matches");
-    
-    if(isMobile===false) {
-        it('should have top ad', function() {
-            expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('scp=pos%3Dtop'); 
-            expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('iu=7175%2Ffdc.forbes%2Fvideo'); 
-            expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('sz=320x50%7C728x90%7C970x250%7C970x90%7C970x66%7C640x360%7C1x1&fluid=height'); 
-        });
-    } else if(isMobile === true) {
-        it('should have mobile ad', function() {
-            expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('scp=pos%3Dmobile'); 
-            expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('iu=7175%2Ffdcmobile%2Fvideo'); 
-            expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('sz=300x50%7C320x50%7C1x1');
-        });
-        
-        it('should have mobilerec ad', function() {
-            expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('scp=pos%3Dmobilerec'); 
-            expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('iu=7175%2Ffdcmobile%2Fvideo'); 
-            expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('sz=300x250%7C300x50%7C320x180%7C320x50%7C1x1');
-        });
-    }
     
 });
