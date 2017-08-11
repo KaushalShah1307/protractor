ad57377ea7cfdac492d74d5ebf302fad4e61ad59
Kaushal Shah <kshah@forbes.com>
2017-08-11 15:53:40 -0400

    check for all the required as sized on videoseries pages

--- test/protractor-spec/videoseries/6.desktopads.spec.js
+++ test/protractor-spec/videoseries/6.desktopads.spec.js
@@ -17,7 +17,7 @@ describe('Ads', function() {
     it('should have topx ad', function() {
         expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('scp=pos%3Dtopx'); 
         expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('iu=7175%2Ffdc.forbes%2Fvideo'); 
-        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('sz=320x50%7C728x90%7C970x66%7C970x250%7C970x90%7C1x1&fluid=height'); 
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('sz=320x50%7C728x90%7C970x250%7C970x90%7C970x66%7C640x360%7C1x1&fluid=height'); 
         expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('specialSlot%3Dcapone'); 
     });
     
