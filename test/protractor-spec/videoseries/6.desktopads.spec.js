e4422b69e3855ab83038a8d136a3a8d91b06ddd1
Kaushal Shah <kshah@forbes.com>
2017-08-10 20:32:36 -0400

    fixed ad calls for series page

--- test/protractor-spec/videoseries/6.desktopads.spec.js
+++ test/protractor-spec/videoseries/6.desktopads.spec.js
@@ -15,10 +15,10 @@ describe('Ads', function() {
     });
     
     it('should have topx ad', function() {
-        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[1].getContentUrl()')).toContain('scp=pos%3Dtopx'); 
-        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[1].getContentUrl()')).toContain('iu=7175%2Ffdc.forbes%2Fvideo'); 
-        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[1].getContentUrl()')).toContain('sz=320x50%7C728x90%7C970x66%7C970x250%7C970x90%7C1x1&fluid=height'); 
-        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[1].getContentUrl()')).toContain('specialSlot%3Dcapone'); 
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('scp=pos%3Dtopx'); 
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('iu=7175%2Ffdc.forbes%2Fvideo'); 
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('sz=320x50%7C728x90%7C970x66%7C970x250%7C970x90%7C1x1&fluid=height'); 
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('specialSlot%3Dcapone'); 
     });
     
 });
--- test/protractor-spec/videoseries/7.mobileads.spec.js
+++ test/protractor-spec/videoseries/7.mobileads.spec.js
@@ -6,14 +6,14 @@ describe('Ads', function() {
         expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('scp=pos%3Dmobile'); 
         expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('iu=7175%2Ffdcmobile%2Fvideo'); 
         expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('sz=320x50%7C300x50%7C320x50%7C1x1&fluid=height');
-        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('specialSlot%3Dcapone');
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[2].getContentUrl()')).toContain('specialSlot%3Dcapone');
     });
         
     it('should have mobilerec ad', function() {
         expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[1].getContentUrl()')).toContain('scp=pos%3Dmobilerec'); 
         expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[1].getContentUrl()')).toContain('iu=7175%2Ffdcmobile%2Fvideo'); 
         expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[1].getContentUrl()')).toContain('sz=320x50%7C300x250%7C300x50%7C320x500%7C320x180%7C320x50%7C1x1&fluid=height');
-        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[0].getContentUrl()')).toContain('specialSlot%3Dcapone');
+        expect(browser.executeScript('return window.Object.values(googletag.pubads().getSlots())[1].getContentUrl()')).toContain('specialSlot%3Dcapone');
     });
         
     it('should have spon-logo ad', function() {
